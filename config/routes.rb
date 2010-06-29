ab00cf9dac7a3332416e16eb300f616992a7a390
Chris Houhoulis <chouhoulis@comcast.net>
2010-06-29 16:50:58 -0400

    RESTfully resourced. Link_tos in show views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :authors
-
-  map.resources :comments
-
-  map.resources :posts
+  map.resources :authors do |auth|
+    auth.resources :posts do |p|
+      p.resources :comments
+    end
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
 
@@ -44,6 +44,6 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+#  map.connect ':controller/:action/:id'
+#  map.connect ':controller/:action/:id.:format'
 end
